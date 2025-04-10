7d73d3d300f768b3b10f96b2049282b7af7112ee
DeokhyeonKim-developer <bitworldexplorer@gmail.com>
2025-04-10 15:27:28 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int x, n, a, b, sum;
+    
+    sum = 0;
+    
+    scanf("%d", &x);
+    scanf("%d", &n);
+    
+    for(int i=0;i<n;++i)
+    {
+        scanf("%d %d", &a, &b);
+        sum = sum + a * b;
+    }
+    
+    if(sum == x)
+        printf("Yes");
+    else
+        printf("No");
+    
+    return 0;
+}
